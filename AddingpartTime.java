ad794bd4c5bdfc2f5f0b1c34225587e638cd0fdf
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-04 20:27:08 +0530

    UC3 Adding part time for emp

---
+++ AddingpartTime.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class AddingpartTime {
+
+	static int wagePerHour = 200;
+
+	public static int dailyWage(int amount) {
+		return (amount * wagePerHour);
+	}
+
+	public static void empIsPresent() {
+		Random random = new Random();
+		int randomNum = random.nextInt(3);
+		if (randomNum == 1) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Day8 Emp Salary computation Problem");
+		empIsPresent();
+	}
+
+}
